a4ea73b73e52d5c04dd9395b22d6e12df0ed55b7
Mopilo <mopahemma@gmail.com>
2019-01-11 00:13:53 +0100

    ui for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import NavBar from '../src/components/layouts//NavBar';
-import Landing from './components/layouts/Landing';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {NavBar} from '../src/components/layouts//NavBar';
+import {Landing} from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login'
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar/>
-        <Landing/>
-      </div>
+      <Router>
+        <div>
+          <NavBar/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </div>
+      </Router>
     );
   }
 }
